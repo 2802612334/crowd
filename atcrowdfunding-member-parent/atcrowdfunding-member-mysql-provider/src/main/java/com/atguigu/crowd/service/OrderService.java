a65b6cff4b49001b64e27bbd9f850a409d77dc8c
root <2802612334@qq.com>
2021-01-24 16:14:18 +0800

    message

---
+++ atcrowdfunding-member-parent/atcrowdfunding-member-mysql-provider/src/main/java/com/atguigu/crowd/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.atguigu.crowd.service;
+
+import com.atguigu.crowd.entity.vo.OrderProjectVO;
+
+public interface OrderService {
+
+    OrderProjectVO getOrderProjectVO(Integer returnId);
+}
