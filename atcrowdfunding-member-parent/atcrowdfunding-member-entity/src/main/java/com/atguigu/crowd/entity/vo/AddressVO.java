62834fcb07e9a548ec5cbfceee72fe884033304d
root <2802612334@qq.com>
2021-01-24 18:39:34 +0800

    模板文件修改

---
+++ atcrowdfunding-member-parent/atcrowdfunding-member-entity/src/main/java/com/atguigu/crowd/entity/vo/AddressVO.java
@@ -0,0 +1,21 @@
+package com.atguigu.crowd.entity.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressVO implements Serializable {
+
+    private Integer addressId;
+
+    private String receiveName;
+
+    private String phoneNum;
+
+    private String address;
+}
