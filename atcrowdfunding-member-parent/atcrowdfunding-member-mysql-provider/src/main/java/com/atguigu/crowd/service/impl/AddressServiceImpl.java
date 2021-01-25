48b0a2963df0cb7a957ed0a11882a4bd459d1730
root <2802612334@qq.com>
2021-01-25 13:51:30 +0800

    模板文件修改

--- atcrowdfunding-member-parent/atcrowdfunding-member-mysql-provider/src/main/java/com/atguigu/crowd/service/impl/AddressServiceImpl.java
+++ atcrowdfunding-member-parent/atcrowdfunding-member-mysql-provider/src/main/java/com/atguigu/crowd/service/impl/AddressServiceImpl.java
@@ -2,10 +2,13 @@ package com.atguigu.crowd.service.impl;
 
 import com.atguigu.crowd.entity.po.AddressPO;
 import com.atguigu.crowd.entity.po.AddressPOExample;
+import com.atguigu.crowd.entity.vo.AddressVO;
 import com.atguigu.crowd.mapper.AddressPOMapper;
 import com.atguigu.crowd.service.AddressService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -23,5 +26,13 @@ public class AddressServiceImpl implements AddressService {
         AddressPOExample.Criteria criteria = example.createCriteria();
         criteria.andMemberIdEqualTo(memberId);
         return addressPOMapper.selectByExample(example);
+    }
+
+    @Override
+    @Transactional(readOnly = false,propagation = Propagation.NESTED,rollbackFor = Exception.class)
+    public void saveAddress(AddressVO addressVO) {
+        AddressPO addressPO = new AddressPO();
+        BeanUtils.copyProperties(addressVO,addressPO);
+        addressPOMapper.insertSelective(addressPO);
     }
 }
